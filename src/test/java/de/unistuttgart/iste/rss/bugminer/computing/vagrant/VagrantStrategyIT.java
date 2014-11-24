61d9b3e354e6a183812ac0d0a42ca13343d8485c
Jan Melcher <info@yogularm.de>
2014-11-24 17:19:30 +0100

    VagrantStrategyIT tests ssh connection

--- src/test/java/de/unistuttgart/iste/rss/bugminer/computing/vagrant/VagrantStrategyIT.java
+++ src/test/java/de/unistuttgart/iste/rss/bugminer/computing/vagrant/VagrantStrategyIT.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.*;
 import static org.junit.Assume.*;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +17,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import de.unistuttgart.iste.rss.bugminer.TestConfig;
 import de.unistuttgart.iste.rss.bugminer.computing.SshConfig;
+import de.unistuttgart.iste.rss.bugminer.computing.SshConnection;
+import de.unistuttgart.iste.rss.bugminer.computing.SshConnector;
 import de.unistuttgart.iste.rss.bugminer.model.Node;
 import de.unistuttgart.iste.rss.bugminer.model.NodeStatus;
 
@@ -24,6 +27,9 @@ import de.unistuttgart.iste.rss.bugminer.model.NodeStatus;
 public class VagrantStrategyIT {
 	@Autowired
 	public VagrantStrategy strategy;
+
+	@Autowired
+	public SshConnector sshConnector;
 
 	@Before
 	public void ensureVagrantIsAvailable() {
@@ -41,6 +47,10 @@ public class VagrantStrategyIT {
 
 			SshConfig sshConfig = strategy.getSshConfig(node);
 			assertThat(sshConfig.getHost(), is("127.0.0.1"));
+
+			SshConnection connection = sshConnector.connect(sshConfig);
+			assertThat(connection.execute(Paths.get("/usr/lib"), "pwd").getOutput(),
+					is("/usr/lib\n"));
 
 			strategy.stopNode(node);
 			assertThat(strategy.getNodeStatus(node), is(NodeStatus.OFFLINE));
