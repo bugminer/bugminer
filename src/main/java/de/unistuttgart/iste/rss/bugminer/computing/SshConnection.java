35661cf18a59b0476f2f9d71aa244ee84d0bc8ac
Jan Melcher <info@yogularm.de>
2014-11-24 17:01:32 +0100

    Run commands on ssh connections

--- src/main/java/de/unistuttgart/iste/rss/bugminer/computing/SshConnection.java
+++ src/main/java/de/unistuttgart/iste/rss/bugminer/computing/SshConnection.java
@@ -5,13 +5,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
 import net.schmizz.sshj.connection.channel.direct.Session.Shell;
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
 
-public class SshConnection implements AutoCloseable {
+import org.apache.commons.io.IOUtils;
+
+import de.unistuttgart.iste.rss.bugminer.utils.ExecutionResult;
+
+public class SshConnection implements AutoCloseable, CommandExecutor {
 	private SSHClient client;
 	private PrintWriter out;
 	private BufferedReader in;
@@ -70,5 +78,31 @@ public class SshConnection implements AutoCloseable {
 	public void close() throws IOException {
 		if (client.isConnected())
 			client.close();
+	}
+
+	@Override
+	public ExecutionResult tryExecute(Path workingDirectory, String... cmd) throws IOException {
+		String cmdString = escapeCommand(cmd);
+		if (workingDirectory != null) {
+			cmdString = escapeCommand("cd", workingDirectory.toString()) + " && " + cmdString;
+		}
+
+		try (Command command = client.startSession().exec(cmdString)) {
+			command.join();
+
+			String stdout = IOUtils.toString(command.getInputStream());
+			String stderr = IOUtils.toString(command.getErrorStream());
+			Integer exitCode = command.getExitStatus();
+			if (exitCode == null) {
+				throw new IOException("Failed to get exit status");
+			}
+			return new ExecutionResult(exitCode, stdout, stderr);
+		}
+	}
+
+	private String escapeCommand(String... cmd) {
+		return Arrays.stream(cmd)
+				.map(c -> "\"" + c + "\"") // TODO escape c
+				.collect(Collectors.joining(" "));
 	}
 }
--- src/test/java/de/unistuttgart/iste/rss/bugminer/computing/SshConnectionTest.java
+++ src/test/java/de/unistuttgart/iste/rss/bugminer/computing/SshConnectionTest.java
@@ -10,11 +10,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Paths;
 
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.Session.Command;
 import net.schmizz.sshj.connection.channel.direct.Session.Shell;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,6 +25,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import de.unistuttgart.iste.rss.bugminer.testutils.TemporaryDirectory;
+import de.unistuttgart.iste.rss.bugminer.utils.ExecutionResult;
 
 
 public class SshConnectionTest {
@@ -103,10 +107,69 @@ public class SshConnectionTest {
 		InputStream shellError = new ByteArrayInputStream(new byte[1000]);
 		when(shell.getErrorStream()).thenReturn(shellError);
 
+		@SuppressWarnings("resource")
 		InteractiveSession shellSession = new SshConnection(config, client).startShell();
 		verify(session).allocateDefaultPTY();
 		assertThat(shellSession.getInputStream(), is(shellInput));
 		assertThat(shellSession.getOutputStream(), is(shellOutput));
 		assertThat(shellSession.getErrorStream(), is(shellError));
+	}
+
+	@Test
+	public void testTryExecute() throws IOException {
+		SshConfig config = new SshConfig("localhost", "me");
+		Session session = mock(Session.class);
+		when(client.startSession()).thenReturn(session);
+		Command command = mock(Command.class);
+		when(session.exec("\"test command\" \"param\"")).thenReturn(command);
+		when(command.getExitStatus()).thenReturn(1);
+		when(command.getInputStream()).thenReturn(IOUtils.toInputStream("output\n"));
+		when(command.getErrorStream()).thenReturn(IOUtils.toInputStream("error line\n"));
+
+		@SuppressWarnings("resource")
+		ExecutionResult result = new SshConnection(config, client).tryExecute(
+				"test command", "param");
+
+		// TODO test escaping
+
+		verify(command).join();
+		assertThat(result.getOutput(), is("output\n"));
+		assertThat(result.getErrorOutput(), is("error line\n"));
+		assertThat(result.getExitCode(), is(1));
+	}
+
+	@SuppressWarnings("resource")
+	@Test
+	public void testTryExecuteWithDirectory() throws IOException {
+		SshConfig config = new SshConfig("localhost", "me");
+		Session session = mock(Session.class);
+		when(client.startSession()).thenReturn(session);
+		Command command = mock(Command.class);
+		when(session.exec("\"cd\" \"/test/dir\" && \"test command\" \"param\""))
+				.thenReturn(command);
+		when(command.getExitStatus()).thenReturn(1);
+		when(command.getInputStream()).thenReturn(IOUtils.toInputStream("output\n"));
+		when(command.getErrorStream()).thenReturn(IOUtils.toInputStream("error line\n"));
+
+		new SshConnection(config, client)
+				.tryExecute(Paths.get("/test/dir"), "test command", "param");
+	}
+
+	@SuppressWarnings("resource")
+	@Test(expected = IOException.class)
+	public void testTryExecuteThrowsWhenExitCodeNotRetrieved() throws IOException {
+		SshConfig config = new SshConfig("localhost", "me");
+		Session session = mock(Session.class);
+		when(client.startSession()).thenReturn(session);
+		when(client.startSession()).thenReturn(session);
+		Command command = mock(Command.class);
+		when(session.exec("\"test command\" \"param\"")).thenReturn(command);
+		when(command.getInputStream()).thenReturn(IOUtils.toInputStream("output\n"));
+		when(command.getErrorStream()).thenReturn(IOUtils.toInputStream("error line\n"));
+
+		// This is the critical line
+		when(command.getExitStatus()).thenReturn(null);
+
+		new SshConnection(config, client).tryExecute("test command", "param");
 	}
 }
