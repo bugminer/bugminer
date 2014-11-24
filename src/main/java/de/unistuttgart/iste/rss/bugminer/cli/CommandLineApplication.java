6e7f9cb62dfbce0fae1bdd3a1fc488b2d4e1aae0
Jan Melcher <info@yogularm.de>
2014-11-24 17:03:57 +0100

    Integrated spring shell framewor

---
+++ src/main/java/de/unistuttgart/iste/rss/bugminer/cli/CommandLineApplication.java
@@ -0,0 +1,11 @@
+package de.unistuttgart.iste.rss.bugminer.cli;
+
+import java.io.IOException;
+
+import org.springframework.shell.Bootstrap;
+
+public class CommandLineApplication {
+	public static void main(String[] args) throws IOException {
+		Bootstrap.main(args);
+	}
+}
