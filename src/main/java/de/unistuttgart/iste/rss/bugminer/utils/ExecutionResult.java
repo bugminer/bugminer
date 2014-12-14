46ffc86ef44819299e6805d66adc7114cddb2642
Jan Melcher <info@yogularm.de>
2014-12-15 00:15:38 +0100

    Make ExecutionResult serializable because it is stored in exceptions

--- src/main/java/de/unistuttgart/iste/rss/bugminer/utils/ExecutionResult.java
+++ src/main/java/de/unistuttgart/iste/rss/bugminer/utils/ExecutionResult.java
@@ -1,16 +1,20 @@
 package de.unistuttgart.iste.rss.bugminer.utils;
 
+import java.io.Serializable;
+
 /**
  * The result of a command execution
  */
-public class ExecutionResult {
+public class ExecutionResult implements Serializable {
+	private static final long serialVersionUID = 6561911779091708307L;
+
 	private final int exitCode;
 	private final String stdout;
 	private final String stderr;
 
 	/**
 	 * Creates a new {@code ExecutionResult}
-	 * 
+	 *
 	 * @param exitCode the exit code of the command, 0 to denote success
 	 * @param stdout the output of the command
 	 * @param stderr the error output of the command
@@ -24,7 +28,7 @@ public class ExecutionResult {
 
 	/**
 	 * Gets the exit code of the command
-	 * 
+	 *
 	 * @return 0 on success, other return values to indicate failure
 	 */
 	public int getExitCode() {
@@ -33,7 +37,7 @@ public class ExecutionResult {
 
 	/**
 	 * Gets the command's output
-	 * 
+	 *
 	 * @return the output
 	 */
 	public String getOutput() {
@@ -42,7 +46,7 @@ public class ExecutionResult {
 
 	/**
 	 * Gets the command's error output
-	 * 
+	 *
 	 * @return the error output
 	 */
 	public String getErrorOutput() {
