e33a64599dcfe90052ad32288b927890ecab1bd4
Benedikt Kersjes <benedikt.kersjes@web.de>
2015-01-27 20:55:37 +0100

    added NotFoundException for rest api

---
+++ src/main/java/de/unistuttgart/iste/rss/bugminer/rest/exceptions/NotFoundException.java
@@ -0,0 +1,12 @@
+package de.unistuttgart.iste.rss.bugminer.rest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// 404
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = -5155390713517541630L;
+
+}
