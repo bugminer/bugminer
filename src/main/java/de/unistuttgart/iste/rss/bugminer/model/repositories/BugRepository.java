fc2a1468e4b291adb7f2129b487e8332c059fe7a
Benedikt Kersjes <benedikt.kersjes@web.de>
2015-01-28 10:58:22 +0100

    BugRepository now extends JpaRepository
    was CrudRepository

--- src/main/java/de/unistuttgart/iste/rss/bugminer/model/repositories/BugRepository.java
+++ src/main/java/de/unistuttgart/iste/rss/bugminer/model/repositories/BugRepository.java
@@ -2,13 +2,13 @@ package de.unistuttgart.iste.rss.bugminer.model.repositories;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import de.unistuttgart.iste.rss.bugminer.model.entities.Bug;
 
 @Repository
-public interface BugRepository extends CrudRepository<Bug, Long> {
+public interface BugRepository extends JpaRepository<Bug, Long> {
 
 	Optional<Bug> findByKey(String key);
 }
