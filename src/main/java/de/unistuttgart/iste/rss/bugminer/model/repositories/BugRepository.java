e2168bf2f29c74aaa87b8f06b7c959512220bf9b
Benedikt Kersjes <benedikt.kersjes@web.de>
2015-01-28 11:02:56 +0100

    extended BugController

--- src/main/java/de/unistuttgart/iste/rss/bugminer/model/repositories/BugRepository.java
+++ src/main/java/de/unistuttgart/iste/rss/bugminer/model/repositories/BugRepository.java
@@ -1,14 +1,18 @@
 package de.unistuttgart.iste.rss.bugminer.model.repositories;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import de.unistuttgart.iste.rss.bugminer.model.entities.Bug;
+import de.unistuttgart.iste.rss.bugminer.model.entities.Project;
 
 @Repository
 public interface BugRepository extends JpaRepository<Bug, String> {
 
 	Optional<Bug> findByKey(String key);
+
+	Collection<Bug> findByProject(Project project);
 }
--- src/main/java/de/unistuttgart/iste/rss/bugminer/rest/BugController.java
+++ src/main/java/de/unistuttgart/iste/rss/bugminer/rest/BugController.java
@@ -1,5 +1,7 @@
 package de.unistuttgart.iste.rss.bugminer.rest;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.unistuttgart.iste.rss.bugminer.model.entities.Bug;
+import de.unistuttgart.iste.rss.bugminer.model.entities.Project;
 import de.unistuttgart.iste.rss.bugminer.model.repositories.BugRepository;
+import de.unistuttgart.iste.rss.bugminer.model.repositories.ProjectRepository;
 import de.unistuttgart.iste.rss.bugminer.rest.exceptions.NotFoundException;
 
 @RestController
@@ -16,9 +20,22 @@ public class BugController {
 	@Autowired
 	private BugRepository bugRepo;
 
+	@Autowired
+	private ProjectRepository projectRepo;
+
 	@RequestMapping(value = "/bug/{key}", method = RequestMethod.GET)
 	public Bug bug(@PathVariable(value = "key") String key) {
 		return bugRepo.findByKey(key).orElseThrow(() -> new NotFoundException());
 	}
 
+	@RequestMapping(value = "/project/{projectId}/bugs", method = RequestMethod.GET)
+	public Collection<Bug> bugsForProject(@PathVariable(value = "projectId") String projectId) {
+		Project project = projectRepo.findOne(projectId);
+
+		if (project == null) {
+			throw new NotFoundException();
+		}
+
+		return bugRepo.findByProject(project);
+	}
 }
