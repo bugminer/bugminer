962ad24f2b336cd4d7337ad2687e365db1cc260b
Benedikt Kersjes <benedikt.kersjes@web.de>
2015-03-03 10:56:23 +0100

    serve static html file from public directory

--- src/main/java/de/unistuttgart/iste/rss/bugminer/Application.java
+++ src/main/java/de/unistuttgart/iste/rss/bugminer/Application.java
@@ -7,7 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import de.unistuttgart.iste.rss.bugminer.model.entities.Bug;
+import de.unistuttgart.iste.rss.bugminer.model.entities.IssueTracker;
+import de.unistuttgart.iste.rss.bugminer.model.entities.Project;
 import de.unistuttgart.iste.rss.bugminer.model.repositories.BugRepository;
+import de.unistuttgart.iste.rss.bugminer.model.repositories.IssueTrackerRepository;
+import de.unistuttgart.iste.rss.bugminer.model.repositories.ProjectRepository;
 
 @Configuration
 @EnableAutoConfiguration
@@ -15,12 +19,27 @@ import de.unistuttgart.iste.rss.bugminer.model.repositories.BugRepository;
 public class Application {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		String[] arguments = new String[1];
+		arguments[0] = "--server.port=8181";
+		ApplicationContext ctx = SpringApplication.run(Application.class, arguments);
 		BugRepository bugRepo = ctx.getBean(BugRepository.class);
+		ProjectRepository projectRepo = ctx.getBean(ProjectRepository.class);
+		IssueTrackerRepository issueTrackerRepo = ctx.getBean(IssueTrackerRepository.class);
+
+		Project project = new Project();
+		project.setName("Bugminer");
+		projectRepo.save(project);
+
+		IssueTracker tracker = new IssueTracker();
+		tracker.setProject(project);
+		tracker.setName("Tracker1");
+		issueTrackerRepo.save(tracker);
 
 		Bug bug = new Bug();
 		bug.setKey("abc");
 		bug.setDescription("Jaja blabla");
+		bug.setProject(project);
+		bug.setIssueTracker(tracker);
 		bugRepo.save(bug);
 	}
 
