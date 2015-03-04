133cce66aeb0effb691d6fcbf9955b06ee31ecf6
Benedikt Kersjes <benedikt.kersjes@web.de>
2015-03-04 19:13:37 +0100

    Show unordered list of projects on index page

--- bugminer-server/src/main/frontend/app/js/app.js
+++ bugminer-server/src/main/frontend/app/js/app.js
@@ -1,4 +1,17 @@
 (function(angular) {
 	'use strict';
+	
+	var app = angular.module('bugminerApp', ['ngResource']);
+	
+	app.factory('Project', function($resource) {
+		  return $resource("/projects/:name");
+	});
+	
+	app.controller('ProjectsCtrl', function($scope, Project) {
+		Project.query(function(data) {
+			$scope.projects = data;
+			console.log(data);
+		});
+	});
 
 })(angular);
