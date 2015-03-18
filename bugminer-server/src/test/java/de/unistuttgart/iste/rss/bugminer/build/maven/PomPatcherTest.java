7f53d5a151e5e71a93702b0247c2a2f3c44beacc
Jan Melcher <info@yogularm.de>
2015-03-18 15:41:20 +0100

    Add tests to PomPaterTest for existing profiles

--- bugminer-server/src/test/java/de/unistuttgart/iste/rss/bugminer/build/maven/PomPatcherTest.java
+++ bugminer-server/src/test/java/de/unistuttgart/iste/rss/bugminer/build/maven/PomPatcherTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 import org.apache.commons.io.IOUtils;
 import org.hamcrest.Matchers;
 
+import static com.jcabi.matchers.RegexMatchers.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
@@ -13,6 +14,13 @@ public class PomPatcherTest extends TestCase {
 		String source = IOUtils.toString(PomPatcherTest.class.getResourceAsStream("pom.xml"));
 		PomPatcher patcher = new PomPatcher();
 		String result = patcher.addCoveragePerTestProfile(source);
-		assertThat(result, containsString("<id>coverage-per-test</id>"));
+		assertThat(result, containsPattern("<profiles>\\s*<profile>\\s*<id>coverage-per-test</id>"));
+	}
+
+	public void testAddCoveragePerTestProfileWhenProfilesAlreadyExists() throws Exception {
+		String source = IOUtils.toString(PomPatcherTest.class.getResourceAsStream("pom-with-profile.xml"));
+		PomPatcher patcher = new PomPatcher();
+		String result = patcher.addCoveragePerTestProfile(source);
+		assertThat(result, containsPattern(".*</profile>\\s*<profile>\\s*<id>coverage-per-test</id>.*"));
 	}
 }
