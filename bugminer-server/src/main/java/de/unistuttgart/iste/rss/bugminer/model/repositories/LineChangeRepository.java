86dada470b9697a80cd69ab82ccb7a7f4c752ac3
Benedikt Kersjes <benedikt.kersjes@web.de>
2015-05-15 12:49:15 +0200

    Added LineChangeRepository

---
+++ bugminer-server/src/main/java/de/unistuttgart/iste/rss/bugminer/model/repositories/LineChangeRepository.java
@@ -0,0 +1,15 @@
+package de.unistuttgart.iste.rss.bugminer.model.repositories;
+
+import de.unistuttgart.iste.rss.bugminer.model.entities.Bug;
+import de.unistuttgart.iste.rss.bugminer.model.entities.LineChange;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+
+@Repository
+public interface LineChangeRepository extends JpaRepository<LineChange, String> {
+
+	List<LineChange> findByBugOrderByFileNameAscOrderByOldLineNumberAscOrderByNewLineNumberIndexAsc(Bug bug);
+}
