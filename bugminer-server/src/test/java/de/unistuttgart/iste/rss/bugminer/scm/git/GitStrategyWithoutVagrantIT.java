05e784bacdb43d4e5f263e972eeee9b0e5499297
Jan Melcher <info@yogularm.de>
2015-05-18 18:27:34 +0200

    Reset context after git tests
    this is because the git repository should be checked out freshly after each
    test method

--- bugminer-server/src/test/java/de/unistuttgart/iste/rss/bugminer/scm/git/GitStrategyWithoutVagrantIT.java
+++ bugminer-server/src/test/java/de/unistuttgart/iste/rss/bugminer/scm/git/GitStrategyWithoutVagrantIT.java
@@ -14,6 +14,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -31,6 +32,7 @@ import static org.junit.Assert.*;
  */
 @ContextConfiguration(classes = TestConfig.class)
 @RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class GitStrategyWithoutVagrantIT {
 	@Autowired
 	private GitStrategy strategy;
