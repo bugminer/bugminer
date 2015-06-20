125df34e2a89c3a1569476f57250c09ffb8b07cb
Jan Melcher <info@yogularm.de>
2015-06-20 12:09:38 +0200

    Add logging output to Task class

--- bugminer-server/src/main/java/de/unistuttgart/iste/rss/bugminer/tasks/Task.java
+++ bugminer-server/src/main/java/de/unistuttgart/iste/rss/bugminer/tasks/Task.java
@@ -1,6 +1,8 @@
 package de.unistuttgart.iste.rss.bugminer.tasks;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.log4j.Logger;
+
 
 public class Task implements Runnable {
 	private String title;
@@ -10,6 +12,8 @@ public class Task implements Runnable {
 	private TaskContext context = new TaskContext();
 	private TaskState state = TaskState.INITIALIZING;
 	private Throwable exception;
+
+	private static final Logger LOGGER = Logger.getLogger(Task.class);
 
 	public Task(String title, TaskRunnable runnable) {
 		this.title = title;
@@ -29,12 +33,15 @@ public class Task implements Runnable {
 			throw new IllegalStateException("Can only run in SCHEDULED or INITIALIZING state, but is " + state);
 		}
 		state = TaskState.RUNNING;
+		LOGGER.info("Task started: " + title);
 		try {
 			runnable.run(context);
 			state = TaskState.FINISHED;
+			LOGGER.info("Task finished: " + title);
 		} catch (Throwable e) {
 			exception = e;
 			state = TaskState.FAILED;
+			LOGGER.warn("Task failed: " + title, exception);
 		}
 	}
 
