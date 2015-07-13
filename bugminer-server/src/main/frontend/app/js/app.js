4e94e8a2819b6cb2aa934fcbf8d0e50d3862a844
Jan Melcher <info@yogularm.de>
2015-07-13 11:34:34 +0200

    Style for tasks

--- bugminer-server/src/main/frontend/app/js/app.js
+++ bugminer-server/src/main/frontend/app/js/app.js
@@ -321,10 +321,46 @@
 		};
 	});
 
-	app.controller('TasksCtrl', function($scope, Task) {
-		Task.query(function(data) {
-			$scope.tasks = data;
+	app.controller('TasksCtrl', function($scope, $timeout, Task) {
+		var cancelled = false;
+		function update() {
+			$timeout(function() {
+				if (cancelled) {
+					return;
+				}
+				Task.query(function(data) {
+					$scope.tasks = data;
+					update();
+				});
+			}, 500);
+		}
+		$scope.$on("$destroy", function() {
+			cancelled = true;
 		});
+		$scope.panelClass = function(state) {
+			var classes = {
+				FINISHED: 'success',
+				FAILED: 'danger'
+			};
+			if (state in classes) {
+				return 'panel panel-' + classes[state];
+			}
+			return 'panel panel-info';
+		};
+		$scope.glyphicon = function(state) {
+			var icons = {
+				INITIALIZING: 'wait',
+				SCHEDULED: 'wait',
+				RUNNING: 'play',
+				FAILED: 'exclamation-sign',
+				FINISHED: 'ok'
+			};
+			if (state in icons) {
+				return 'glyphicon glyphicon-' + icons[state];
+			}
+			return '';
+		};
+		update();
 	});
 
 })(angular);
